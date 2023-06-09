e0f83c89c5afb1149c5be17a9853f6e3d9bfe01b
Anthony Ramos <ramosanthony@gmail.com>
2023-06-09 14:00:14 -0400

    Left Arrow and Image Array Bug

--- src/Carousel.test.js
+++ src/Carousel.test.js
@@ -26,3 +26,39 @@ it("works when you click on the right arrow", function() {
   expect(queryByAltText("Photo by Richard Pasquarella on Unsplash")).not.toBeInTheDocument();
   expect(queryByAltText("Photo by Pratik Patel on Unsplash")).toBeInTheDocument();
 });
+
+it("works when you click on the left arrow", function () {
+  const { queryByTestId, queryByAltText } = render(<Carousel />);
+
+  // move forward in the carousel
+  const rightArrow = queryByTestId("right-arrow");
+  fireEvent.click(rightArrow);
+
+  // expect the second image to show, but not the first
+  expect(queryByAltText("Photo by Richard Pasquarella on Unsplash")).not.toBeInTheDocument();
+  expect(queryByAltText("Photo by Pratik Patel on Unsplash")).toBeInTheDocument();
+
+  // move backward in the carousel
+  const leftArrow = queryByTestId("left-arrow");
+  fireEvent.click(leftArrow);
+
+  // expect the first image to show, but not the second
+  expect(queryByAltText("Photo by Pratik Patel on Unsplash")).not.toBeInTheDocument();
+  expect(queryByAltText("Photo by Richard Pasquarella on Unsplash")).toBeInTheDocument();
+});
+
+it("hides the left arrow at first image in carousel", function () {
+  const { queryByTestId } = render(<Carousel />);
+
+  expect(queryByTestId("left-arrow")).not.toBeVisible();
+});
+
+it("hides the right arrow at last image in carousel", function () {
+  const { queryByTestId } = render(<Carousel />);
+  // move forward in the carousel to end
+  const rightArrow = queryByTestId("right-arrow");
+  fireEvent.click(rightArrow);
+  fireEvent.click(rightArrow);
+  
+  expect(queryByTestId("right-arrow")).not.toBeVisible();
+});
